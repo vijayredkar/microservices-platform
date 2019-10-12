4b1bd35288c152d6093550dee69e6b4777a90309
Vijay Redkar <vijay.redkar@dnata.com>
2019-10-12 11:50:02 +0400

    Create Distributed Log Tracing base

---
+++ log-tracing/src/main/java/com/company/ecom/logtracing/LogTracingApplication.java
@@ -0,0 +1,14 @@
+package com.company.ecom.logtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableZipkinServer
+public class LogTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTracingApplication.class, args);
+	}
+
+}
