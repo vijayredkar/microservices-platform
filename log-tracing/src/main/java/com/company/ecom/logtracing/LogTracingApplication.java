f4b8e873891f7001cfc241501f3b3d8be0fff993
Vijay Redkar <vijay.redkar@dnata.com>
2019-10-17 00:02:07 +0400

    Log tracing config with Sleuth Zipkin

--- log-tracing/src/main/java/com/company/ecom/logtracing/LogTracingApplication.java
+++ log-tracing/src/main/java/com/company/ecom/logtracing/LogTracingApplication.java
@@ -2,9 +2,10 @@ package com.company.ecom.logtracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-//@EnableZipkinServer
+@EnableZipkinServer
 public class LogTracingApplication {
 
 	public static void main(String[] args) {
