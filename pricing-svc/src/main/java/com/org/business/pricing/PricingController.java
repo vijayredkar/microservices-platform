4c3216f6870554b23e9ba1f379a320860f77db5c
Vijay Redkar <vijay.redkar@dnata.com>
2019-10-16 23:54:03 +0400

    Pricing Service Log manage with Sleuth Zipkin

--- pricing-svc/src/main/java/com/org/business/pricing/PricingController.java
+++ pricing-svc/src/main/java/com/org/business/pricing/PricingController.java
@@ -2,19 +2,25 @@ package com.org.business.pricing;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/pricing")
 public class PricingController {
-    
+  
+	Logger logger = LoggerFactory.getLogger(PricingController.class);
+	
     @GetMapping("/items")
     @HystrixCommand(fallbackMethod="getDefaultPricing")
     public String getPricing()
     {
         System.out.println("---------------- pricing controller ");
+        logger.info("---------------- pricing controller log");
         /*String a= null;
         a.length();*/
         
@@ -24,6 +30,7 @@ public class PricingController {
     public String getDefaultPricing()
     {
         System.out.println("---------------- pricing defaulted ");
+        logger.info("---------------- pricing defaulted log");
         return "pricing defaulted";
     }
 
